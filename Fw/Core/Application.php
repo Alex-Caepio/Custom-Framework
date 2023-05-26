b564442904f1deb4c54f4deeaa1d31380e249932
Aleksei Brutov <mjbcaepio@gmail.com>
2023-05-26 14:07:18 +0300

    Изменил класс Config, добавив в него функцию init.

--- Fw/Core/Application.php
+++ Fw/Core/Application.php
@@ -2,13 +2,16 @@
 
 namespace Fw\Core;
 
+
 class Application
 {
     private $pager = null; // будет объект класса
     private static $instance = null;
     private $template = null;//будет объект класса
 
-    private function __construct() {}
+    private function __construct()
+    {
+    }
 
     public static function getInstance()
     {
--- Fw/Core/Config.php
+++ Fw/Core/Config.php
@@ -2,14 +2,23 @@
 
 namespace Fw\Core;
 
+
 class Config
 {
     private static $config;
+
+    public static function init()
+    {
+        self::$config = require __DIR__ . '/../config.php';
+    }
+
     public static function get(string $path)
     {
+        if (is_null(self::$config)) {
+            self::init();
+        }
         $variables = explode('/', $path);
         $config = self::$config;
-
         foreach ($variables as $var) {
             if (isset($config[$var])) {
                 $config = $config[$var];
